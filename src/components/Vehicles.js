57bc96c42f4c44c2be875b2527e83d668d7a3387
♿ <biznes@2.pl>
2022-02-09 09:35:23 +0100

    cv

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,29 +1,25 @@
 import { useState } from 'react';
 import { latLng } from 'leaflet';
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useMapEvents, useMap } from 'react-leaflet';
 import VehicleMarker from './VehicleMarker';
 import ActiveVehicle from './ActiveVehicle';
-import { nearestPointOnLine, lineString, point } from '@turf/turf';
 
 export default function Vehicles({ vehicles }) {
     const map = useMap();
     const [ bounds, setBounds ] = useState(map.getBounds());
-    const [ active, setActive ] = useState(null);
-    const [ trip, setTrip ] = useState(null);
 
     return (
         <>
-            {vehicles.filter(vehicle => {
-                if(active) return vehicle.tab === active.tab && vehicle.type === active.type;
-                return bounds.contains(latLng(vehicle.location)) && map.getZoom() > 15
-            }).map(vehicle => (
-                <VehicleMarker key={vehicle.trip} vehicle={vehicle} clickCallback={() => {
-                    setActive(vehicle);
-                    loadTrip(vehicle.trip, vehicle);
-                }} />
-            ))}
-            {active && trip ? <ActiveVehicle vehicle={[ active, setActive ]} trip={[ trip, setTrip ]} /> : null}
-            <Events />
+            <Routes>
+                <Route path="/" element={<>
+                    {vehicles.filter(vehicle => bounds.contains(latLng(vehicle.location)) && map.getZoom() > 15).map(vehicle => (
+                        <VehicleMarker key={vehicle.trip} vehicle={vehicle} clickCallback={() => <Navigate to={`/${vehicle.tab}`} />} />
+                    ))}
+                    <Events />
+                </>} />
+                <Route path="/track/:type/:bus" element={<ActiveVehicle vehicles={vehicles} />} />
+            </Routes>
         </>
     );
 
@@ -36,16 +32,5 @@ export default function Vehicles({ vehicles }) {
             }
         });
         return null;
-    }
-
-    async function loadTrip(trip_id, vehicle) {
-        if(vehicle) setActive(vehicle);
-        let res = await fetch(`https://api.domeqalt.repl.co/trip?trip=${trip_id}`).then(res => res.json())
-        res.stops = res.stops.map(stop => {
-            stop.onLine = nearestPointOnLine(lineString(res.shapes), point(stop.location), { units: 'meters' }).properties.location;
-            return stop;
-        });
-        setTrip(res);
-        console.log(res)
     }
 }
