6d7e92695f67be3bd930b9030cd362d478b9a2be
DomeQdev <biznes@2.pl>
2022-02-10 00:16:14 +0100

    Update ActiveVehicle.js

--- src/components/ActiveVehicle.js
+++ src/components/ActiveVehicle.js
@@ -63,7 +63,7 @@ export default function ActiveVehicle({ vehicles }) {
                             <div>
                                 <Button variant="outlined" style={{ color: "#000000", borderColor: vehicle?.type === "bus" ? "#006b47" : "#007bff" }}>{vehicle?.type === "bus" ? <DirectionsBus style={{ height: "22px", width: "22px", fill: "#006b47" }} /> : <Tram style={{ height: "22px", width: "22px", fill: "#007bff" }} />} <b>{trip?.route_id}</b>&nbsp;{trip?.trip_headsign} {trip?.wheelchair_accessible ? <Accessible style={{ height: "22px", width: "22px" }} /> : <NotAccessible style={{ height: "22px", width: "22px" }} />}</Button>
                             </div>
-                            <div>{Math.floor((Date.now() - vehicle?.lastPing) / 1000)}s temu</div>
+                            <div></div>
                         </Box>
                         <List
                             sx={{
