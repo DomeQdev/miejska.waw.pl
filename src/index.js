bdc316789e1d3feec393ef44feddda48e164c8cb
DomeQdev <biznes@2.pl>
2022-02-11 10:12:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AddToHomescreen from 'react-add-to-homescreen';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './App.css';
 
 ReactDOM.render(
-  <AddToHomescreen onAddToHomescreenClick={() => {}} />
   <BrowserRouter>
     <App />
   </BrowserRouter>,
