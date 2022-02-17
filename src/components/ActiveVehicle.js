d787d77eb876c4190774a0a8cb5c4a87120c25bd
♿ <biznes@2.pl>
2022-02-17 16:27:51 +0100

    wyposażenie, literówka

--- src/components/ActiveVehicle.js
+++ src/components/ActiveVehicle.js
@@ -85,13 +85,19 @@ export default function ActiveVehicle({ vehicles }) {
                             </div> 
                         </Box>
                         {displayingVehicle ? 
-                            <div style={{ maxHeight: "315px", WebkitOverflowScrolling: "touch" }}>
+                            <div style={{ maxHeight: "315px", WebkitOverflowScrolling: "touch", paddingLeft: "20px", overflow: "auto" }}>
                                 <b>Pojazd:</b> {vehicleInfo?.brand} {vehicleInfo?.model}<br />
                                 <b>Rok produkcji:</b> {vehicleInfo?.prodYear}<br />
-                                <b>Number boczny:</b> {vehicle?.tab}<br />
+                                <b>Numer boczny:</b> {vehicle?.tab}<br />
                                 {vehicleInfo?.registration ? <><b>Numer rejestracyjny:</b> {vehicleInfo?.registration}<br /></> : null}
-                                {vehicleInfo?.carrier ? <><b>Przewoźnik:</b> {vehicleInfo?.carrier}<br /></> : null}
+                                {vehicleInfo?.carrier ? <><b>Przewoznik:</b> {vehicleInfo?.carrier}<br /></> : null}
                                 {vehicleInfo?.depot ? <><b>Zajezdnia:</b> {vehicleInfo?.depot}<br /></> : null}
+                                {vehicleInfo?.features?.length ? <><b>Wyposazenie:</b> {vehicleInfo?.features?.join(", ")}<br /></> : null}
+                                <p style={{height: "10px"}} />
+                                <div style={{ textAlign: "center" }}>
+                                    <h3>Podoba ci sie projekt?</h3>
+                                    <p>Powiedz o nim znajomym! Strona jest w 100% darmowa i wolna od reklam!</p>
+                                </div>
                             </div> : <List
                             sx={{
                                 overflow: "auto",
